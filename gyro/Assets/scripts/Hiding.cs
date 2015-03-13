8249529f22c36a316112350cdca84eb61de1057b
TomZoy <TomZoy@users.noreply.github.com>
2015-03-13 17:27:07 +0000

    SafeZone Implemented (it reports if player is there)

--- gyro/Assets/scripts/Hiding.cs
+++ gyro/Assets/scripts/Hiding.cs
@@ -4,6 +4,18 @@ using System.Collections;
 namespace OTM {
 public class Hiding : MonoBehaviour {
 
+		private bool outerCorssed = true;
+		public bool innerStaying = false;
+
+
+	public void flipOuterCorssed()
+	{
+			outerCorssed = !outerCorssed;
+	}
+
+	
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,8 +23,24 @@ public class Hiding : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+			if ((outerCorssed)&&(innerStaying))
+			{
+				Debug.LogWarning ("you stepped in the safe zone!");
+			}
+
+
 	}
+
+
+		void OnTriggerExit2D(Collider2D other) {
+			
+			if (other.gameObject.CompareTag("Player")) {
+				
+
+			}
+		}
+
 
 
 
---
+++ gyro/Assets/scripts/Hiding_reporter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OTM {
+public class Hiding_reporter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		
+		if ((other.gameObject.CompareTag ("Player")) && (this.gameObject.name == "outer")) {
+			this.GetComponentInParent<Hiding> ().flipOuterCorssed ();
+
+			Debug.Log (" outer exit");
+		}
+
+		if ((other.gameObject.CompareTag("Player")) && (this.gameObject.name=="inner")) {
+				this.GetComponentInParent<Hiding>().innerStaying = false;
+				
+				Debug.Log("staying");
+		}
+	}
+
+		void OnTriggerEnter2D(Collider2D other) {
+			
+			if ((other.gameObject.CompareTag("Player")) && (this.gameObject.name=="outer")) {
+				this.GetComponentInParent<Hiding>().flipOuterCorssed();
+				
+				Debug.Log(" outer enter");
+			}
+		}
+
+		void OnTriggerStay2D(Collider2D other) {
+			
+			if ((other.gameObject.CompareTag("Player")) && (this.gameObject.name=="inner")) {
+				this.GetComponentInParent<Hiding>().innerStaying = true;
+				
+				Debug.Log("staying");
+			}
+		}
+
+}
+}
